bdfa7f57dd7c662a4188e91ddc87f74353c8c42f
Dalaikhvv <dd4184968@gmail.com>
2022-11-28 17:19:40 +0800

    half

---
+++ Javascript.js/any/Variable.js
@@ -0,0 +1,11 @@
+// 4
+let message = "hello";
+console.log(message); // "string"
+message = true;
+console.log(message); // "boolean"
+message = 100;
+console.log(message); // "number"
+let name;
+console.log(name); // "undefined"
+let test = null;
+console.log(test); // "undefined"
