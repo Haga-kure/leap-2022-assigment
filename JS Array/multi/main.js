3d785686dfb9518bca0c9bc9176b4865e281fb62
Dalaikhvv <dd4184968@gmail.com>
2022-12-07 20:02:32 +0800

    half

---
+++ JS Array/multi/main.js
@@ -0,0 +1,11 @@
+const data = [4, 2, 34, 4, 1, 12, 1, 4];
+let count = 0;
+let a = 0;
+debugger;
+for (let i = 0; i < data.length; ) {
+  a++;
+  if (data[i] === data[a]) {
+  } else {
+    console.log(data[i]);
+  }
+}
