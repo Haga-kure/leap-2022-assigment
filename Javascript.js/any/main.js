20ce8c8c7e0a5993cc5e91e9fbb2839675570a7f
Dalaikhvv <dd4184968@gmail.com>
2022-12-15 18:23:45 +0800

    assigment

---
+++ Javascript.js/any/main.js
@@ -0,0 +1,9 @@
+// 1
+const hi = "hello world";
+console.log(hi);
+// 2
+const messsage = "Javascript lesson begin";
+console.log(messsage);
+// 3
+const olonMor = "Comments can make \ncode readable";
+console.log(olonMor);
---
+++ Js condition - assigment/huvisagch/main.js
@@ -0,0 +1,39 @@
+//1
+let a = 85;
+let b = 75;
+let c = 96;
+let d = 69;
+let n = 0;
+if (a > 80) {
+  n = n + a;
+}
+if (b > 80) {
+  n = n + b;
+}
+if (c > 80) {
+  n = n + c;
+}
+if (d > 80) {
+  n = n + d;
+}
+console.log(n);
+//2
+let j = 3;
+let k = 7;
+let l = 2;
+let i = 4;
+let niit = 0;
+if (j < 5) {
+  niit *= a;
+}
+if (k < 5) {
+  niit *= b;
+}
+if (l < 5) {
+  niit *= c;
+}
+if (i < 5) {
+  niit *= d;
+}
+
+console.log(niit);
